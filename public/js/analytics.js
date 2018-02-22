755dedac7a2d7f15fb19d65d15589ac497a30fa5
DESKTOP-JOUQUFE\Devart creativity <devart.creativity@gmail.com>
2018-02-22 17:20:53 +0100

    x

--- public/js/analytics.js
+++ public/js/analytics.js
@@ -1,5 +1,3 @@
-//analytics
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-84124698-1"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
